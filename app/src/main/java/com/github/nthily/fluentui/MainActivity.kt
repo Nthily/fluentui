c5b9dc7f5549e142f77a94b6a5a33ac41695d3a1
Nthily <nthily@outlook.com>
2022-04-17 00:31:34 +0800

    initial commit

---
+++ app/src/main/java/com/github/nthily/fluentui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.github.nthily.fluentui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.github.nthily.fluentui.ui.theme.FluentUiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FluentUiTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/github/nthily/fluentui/ui/components/Avatar.kt
@@ -0,0 +1,8 @@
+package com.github.nthily.fluentui.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Avatar() {
+
+}
